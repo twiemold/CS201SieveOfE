eaf7d051953d648b98a3e55eaf6186d8503fecb2
twiemold <twiemold@uvm.edu>
2022-04-05 20:37:54 -0400

    Refactored constant names

--- sieveofe.twiemold.c
+++ sieveofe.twiemold.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <math.h>
-#define M 200
-#define S floor(sqrt(M)) + 1
+#define arrayMax 200
+#define stopValue floor(sqrt(arrayMax)) + 1
 // TODO: 0 indexing problem?
 
 int main() {
     char done = 0;
-    char nums[M];
-    for (int i = 2; i < M; ++i) {
+    char nums[arrayMax];
+    for (int i = 2; i < arrayMax; ++i) {
         nums[i] = 1;
     }
 //    while not done {
     while (!(done)) {
         char pPicked = 0;
-        // let p be the smallest integer less than or equal to S
+        // let p be the smallest integer less than or equal to stopValue
         // and not yet crossed off and not marked as visited
         int p = 2;
-        for (int i = 2; i <= S; ++i) {
+        for (int i = 2; i <= stopValue; ++i) {
             if (nums[i] != 0 && nums[i] != -1) {
                 p = i;
                 pPicked = 1;
@@ -26,16 +26,16 @@ int main() {
         // done = 1
         // } else {
         // mark p as visited
-        // cross off all multiples of p, starting at 2*p and ending at M
+        // cross off all multiples of p, starting at 2*p and ending at arrayMax
         if (!(pPicked)) {
             pPicked = 1;
             done = 1;
         } else {
             nums[p] = -1;
-            // cross off all multiples of p, starting at 2*p and ending at M
+            // cross off all multiples of p, starting at 2*p and ending at arrayMax
             int val = 2;
             int multiple = val * p;
-            while (multiple < M) {
+            while (multiple < arrayMax) {
                 nums[multiple] = 0;
                 val++;
                 multiple = val * p;
@@ -44,7 +44,7 @@ int main() {
     }
 
     int count = 0;
-    for (int i = 0; i < M; ++i) {
+    for (int i = 0; i < arrayMax; ++i) {
         if (nums[i] == 1 || nums[i] == -1) {
             count++;
         }
